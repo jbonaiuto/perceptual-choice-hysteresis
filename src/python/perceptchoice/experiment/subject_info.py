f35ad119ae20ed3dc4e50d171fef2d6328bb9a94
jbonaiuto <jbonaiuto@gmail.com>
2016-11-14 11:16:43 +0000

    added analysis of iti experiment

--- src/python/perceptchoice/experiment/subject_info.py
+++ src/python/perceptchoice/experiment/subject_info.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import math
 import os
 import numpy as np
+import sys
 from perceptchoice.utils import mdm_outliers
 
 LEFT=180
@@ -133,6 +135,67 @@ subject_sessions={
 }
 
 
+class ISISubject:
+    def __init__(self, data_dir, subj_id):
+        """
+        Initialize subject data
+        """
+        self.data_dir=data_dir
+        self.id=subj_id
+        self.session_data={}
+
+    def read_data(self):
+        # Figure out sessions - look for data files with subject ID in start of the name
+        run_files={}
+        for file_name in os.listdir(self.data_dir):
+            if file_name.lower().endswith('.csv'):
+                file_name_parts=file_name.split('.')
+                if file_name_parts[0].upper()==self.id:
+                    if not file_name_parts[2]=='training':
+                        run=int(file_name_parts[2])
+                        run_files[run]=file_name
+        runs=sorted(run_files.keys())
+        # Iterate through subject sessions
+        trial_data=[]
+        for run in runs:
+            file=open(os.path.join(self.data_dir,run_files[run]))
+
+            # Initialize trial data
+            # Initialize previous resp, RT and trial coherence
+            last_resp=float('NaN')
+            last_coherence=float('NaN')
+            for line_idx,line in enumerate(file):
+                if line_idx>0:
+                    cols=line.split(',')
+
+                    # Extract direction
+                    direction=int(cols[0])
+                    if direction==LEFT:
+                        direction=-1
+                    elif direction==RIGHT:
+                        direction=1
+                    coherence=float(cols[1])
+                    isi=int(cols[2])
+                    correct=int(cols[3])
+                    # Determine response based on whether or not correct and the direction
+                    resp=direction
+                    if correct<1:
+                        resp=direction*-1
+                        # Convert RT to ms
+                    rt=float(cols[4])*1000.0
+                    trialIdx=line_idx-1
+                    # If a response was made
+                    if coherence in coherences and rt<=982:
+                        trial_data.append([trialIdx, direction, coherence, correct, resp, last_resp, rt, isi, last_coherence])
+                        # Update last trial respnse, RT and coherence
+                    last_resp=resp
+                    last_coherence=coherence
+        # Remove outliers based on RT
+        trial_data=np.array(trial_data)
+        outliers=mdm_outliers(trial_data[:,6])
+        trial_data=trial_data[np.setdiff1d(np.array(range(trial_data.shape[0])),np.array(outliers)),:]
+        self.session_data['control']=trial_data
+
 class Subject:
 
     def __init__(self, data_dir, subj_id):
@@ -203,7 +266,10 @@ class Subject:
                                         rt=float(cols[3])*1000.0
                                         trialIdx=line_idx-1
                                         # Compute ITI
-                                        iti=1000+1000-last_rt
+                                        if math.isnan(last_rt) or last_rt>982:
+                                            iti=1000
+                                        else:
+                                            iti=1000+1000-last_rt
                                         # If a response was made
                                         if coherence in coherences and rt<=982:
                                             trial_data.append([trialIdx, direction, coherence, correct, resp, last_resp, rt, iti, last_coherence])
@@ -226,3 +292,11 @@ def read_subjects(data_dir):
         subjects[subj_id].read_data()
     return subjects
 
+def read_isi_subjects(data_dir):
+    subjects={}
+    for subj_id in range(1,25):
+        # Exclude 16 because overall accuracy threshold is >.25 (its .54!)
+        if not subj_id==16:
+            subjects[subj_id]=ISISubject(data_dir, '%d' % subj_id)
+            subjects[subj_id].read_data()
+    return subjects
